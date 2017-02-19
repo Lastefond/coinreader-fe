73c35fdf7419f4a71df234cb43cda2f1c382aa86
Kaupo Juhkam <kaupo@credy.eu>
2017-02-19 15:44:04 +0200

    now POSTing coins too, reset counters after submit

--- config/params.php
+++ config/params.php
@@ -2,4 +2,7 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'coinReader' => [
+        'url' => 'ws://0.0.0.0:8080/coins',
+    ],
 ];
--- views/site/index.php
+++ views/site/index.php
@@ -5,8 +5,9 @@
 use yii\helpers\Url;
 
 $this->title = 'My Yii Application';
+$coinreaderUrl = Yii::$app->params['coinReader']['url'];
 $this->registerJs(<<<JS
-    var socketUrl = '0.0.0.0:8080/coins';
+    var socketUrl = '{$coinreaderUrl}';
     var coins = [];
     var sum = 0;
     var coinMap = {
@@ -18,25 +19,25 @@ $this->registerJs(<<<JS
         6: 5
     };
 
-    $('body').on('submit', '#coinform', function (e) {
-        e.preventDefault();
-        $.post( $('#coinform').attr('action'), $('#coinform').serialize(), function( data, hue, xhr ) {
-            if (xhr.status == 202) {
-                // TODO no coins entered
-                alert(data.message);
-            }
-        }).error(function(data) {
-            // TODO handle better
-            alert(data);
-        });
-    });
-
     var mapCoin = function (value) {
-        return coinMap[value];
+        return coinMap[value] || 0;
+    };
+    
+    var resetCounters = function () {
+        coins = [];
+        sum = 0;
+        addCoin(0);
+    };
+    
+    var addCoin = function(coinValue) {
+        coins.push(coinValue);
+        sum += parseInt(coinValue);
+        var eur = sum / 100;
+        $('#sum').text(eur.toFixed(2) + ' €');
     };
 
     function start(websocketServerLocation){
-        var connection = new WebSocket('ws://' + websocketServerLocation);
+        var connection = new WebSocket(websocketServerLocation);
 
         // When the connection is open, send some data to the server
         connection.onopen = function () {
@@ -50,16 +51,40 @@ $this->registerJs(<<<JS
 
         // Log messages from the server
         connection.onmessage = function (e) {
-            var coinValue = mapCoin(e.data);
-            coins.push(coinValue);
-            sum += parseInt(coinValue);
-            var eur = sum / 100;
-            $('#sum').text(eur.toFixed(2) + ' €');
+            addCoin(mapCoin(e.data));
         };
     }
 
     start(socketUrl);
     
+    $('body').on('submit', '#coinform', function (e) {
+        e.preventDefault();
+        var coinData = [];
+        coins.forEach(function (coin, idx) {
+            coinData[idx] = {coin_value: coin};
+        });
+
+        var formData = {
+            User: {
+                first_name: $('#f_name').val(),
+                last_name: $('#l_name').val()
+            },
+            Coin: coinData
+        };
+        
+        $.post( $('#coinform').attr('action'), formData, function( data, hue, xhr ) {
+            if (xhr.status == 200) {
+                resetCounters();
+            }
+            else if (xhr.status == 202) {
+                // TODO no coins entered
+                alert(data.message);
+            }
+        }).error(function(data) {
+            // TODO handle better
+            alert(data);
+        });
+    });
 JS
 )
 ?>
@@ -71,11 +96,11 @@ JS
         <form id="coinform" action="<?= Url::to(['/coin']) ?>" method="post">
             <div class="form-group">
                 <label>Eesnimi</label>
-                <input class="form-control" name="User[first_name]">
+                <input id="f_name" class="form-control" name="User[first_name]">
             </div>
             <div class="form-group">
                 <label>Perekonnanimi</label>
-                <input class="form-control" name="User[last_name]">
+                <input id="l_name" class="form-control" name="User[last_name]">
             </div>
 
             <button type="submit" class="btn btn-lg btn-success">Yes, send money!</button>
