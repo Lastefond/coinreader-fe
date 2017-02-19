0ec0bfbf191fd473976a9ecf9b4b9401f052a522
Kaupo Juhkam <kaupo@credy.eu>
2017-02-19 16:26:33 +0200

    add db to shared files

--- deploy.php
+++ deploy.php
@@ -8,7 +8,9 @@ require 'recipe/yii.php';
 
 // Set configurations
 set('repository', 'https://github.com/Lastefond/coinreader-fe.git');
-
+set('shared_files', [
+    'runtime/db.sqlite',
+]);
 task('deploy:fix_assets', function () {
     run('chmod 777 {{release_path}}/web/assets');
 });
