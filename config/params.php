b2ad225b7fdd5ea7e6fd945bd71d31da74a81d32
Kaupo Juhkam <kaupo@credy.eu>
2017-02-21 22:48:42 +0200

    cleanup

--- config/params.php
+++ config/params.php
@@ -5,8 +5,4 @@ return [
     'coinReader' => [
         'url' => 'ws://127.0.0.1:8080/coins',
     ],
-    'coinSender' => [
-        'apiUrl' => getenv('COIN_API_URL'),
-        'boxId' => getenv('COIN_BOX_ID'),
-    ],
 ];
