6ffd3ac200007a026c49ac1e0617c837c63d6902
Airon R <airon@wisemedia.ee>
2017-03-04 17:36:30 +0200

    Testing zoom disable

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,7 +13,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
