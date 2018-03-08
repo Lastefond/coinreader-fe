8e1ac2aaf1cd38466eb476cbe45284f6e36d07ee
Airon Roosalu <airon@credy.eu>
2018-03-09 00:28:05 +0200

    Reverted zoom fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,7 +13,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
